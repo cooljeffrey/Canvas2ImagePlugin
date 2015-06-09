49d8f8de2bc37a4654c625116fa1cb7ca39b934b
jeffrey <cooljeffrey@gmail.com>
2015-06-10 00:01:38 +0800

    ios supports save image to specified album

--- src/ios/Canvas2ImagePlugin.m
+++ src/ios/Canvas2ImagePlugin.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
 //	MIT Licensed
 //
+//Updated to return file path in success callback
 
 #import "Canvas2ImagePlugin.h"
 #import <Cordova/CDV.h>
@@ -22,34 +23,76 @@
 - (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
 {
     self.callbackId = command.callbackId;
-	NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
-	
-	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];	
-	UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-	
+    NSString* imageDataUrl = [command.arguments objectAtIndex:0];
+    NSString* folderName = [command.arguments objectAtIndex:1];
+//    NSString* filename = [command.arguments objectAtIndex:2];
+
+	NSData* imageData = [NSData cdv_dataFromBase64String: imageDataUrl];
+	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];
+    
+	ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    
+    __block ALAssetsGroup* groupToAddTo;
+    [library enumerateGroupsWithTypes:ALAssetsGroupAlbum
+       usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+           if ([[group valueForProperty:ALAssetsGroupPropertyName] isEqualToString:folderName]) {
+               NSLog(@"found album %@", folderName);
+               groupToAddTo = group;
+               [self saveImageToAlbum:library image:image groupToAddTo:groupToAddTo command:command];
+           }
+       }
+     failureBlock:^(NSError* error) {
+         NSLog(@"failed to enumerate albums:\nError: %@", [error localizedDescription]);
+         [library addAssetsGroupAlbumWithName:folderName
+          resultBlock:^(ALAssetsGroup *group) {
+              NSLog(@"added album:%@", folderName);
+              [self saveImageToAlbum:library image:image groupToAddTo:group command:command];
+          }
+         failureBlock:^(NSError *error) {
+             NSLog(@"error adding album");
+             [library release];
+             CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+             [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+         }];
+     }];
 }
 
-- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+- (void) saveImageToAlbum:(ALAssetsLibrary*) library image:(UIImage*) image groupToAddTo:(ALAssetsGroup*) groupToAddTo command:(CDVInvokedUrlCommand*)command
 {
-    // Was there an error?
-    if (error != NULL)
-    {
-        // Show error message...
-        NSLog(@"ERROR: %@",error);
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-		[self.webView stringByEvaluatingJavaScriptFromString:[result toErrorCallbackString: self.callbackId]];
-    }
-    else  // No errors
-    {
-        // Show message image successfully saved
-        NSLog(@"IMAGE SAVED!");
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
-		[self.webView stringByEvaluatingJavaScriptFromString:[result toSuccessCallbackString: self.callbackId]];
-    }
+    [library writeImageToSavedPhotosAlbum: image.CGImage metadata:nil completionBlock:^(NSURL *assetURL, NSError *error){
+        if (error)
+        {
+            // Show error message...
+            NSLog(@"ERROR: %@",error);
+            [library release];
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }
+        else
+        {
+            // Show message image successfully saved
+            NSLog(@"Saved URL : %@", assetURL);
+            // try to get the asset
+            [library assetForURL:assetURL
+                     resultBlock:^(ALAsset *asset) {
+                         // assign the photo to the album
+                         [groupToAddTo addAsset:asset];
+                         NSLog(@"Added %@ to %@", assetURL, [groupToAddTo valueForProperty:ALAssetsGroupPropertyName]);
+                         [library release];
+                         CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:[assetURL absoluteString]];
+                         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                     }
+                    failureBlock:^(NSError* error) {
+                        NSLog(@"failed to retrieve image asset:\nError: %@ ", [error localizedDescription]);
+                        [library release];
+                        CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+                        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                    }];
+        }
+    }];
 }
-
 - (void)dealloc
-{	
+{
 	[callbackId release];
     [super dealloc];
 }
